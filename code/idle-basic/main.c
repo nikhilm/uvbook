dd968b45755306cca66f282c4334759db13e7bcd
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-06-18 15:39:09 +0530

    Described event loops and watchers in libuv
    Added idle-basic example of running the loop.
    Described reference counting

---
+++ code/idle-basic/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <uv.h>
+
+int64_t counter = 0;
+
+void wait_for_a_while(uv_idle_t* handle, int status) {
+    counter++;
+
+    if (counter >= 10e6)
+        uv_idle_stop(handle);
+}
+
+int main() {
+    uv_idle_t idler;
+
+    uv_idle_init(uv_default_loop(), &idler);
+    uv_idle_start(&idler, wait_for_a_while);
+
+    printf("Idling...\n");
+    uv_run(uv_default_loop());
+
+    return 0;
+}
