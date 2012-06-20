d22bc5da402ba8dafc8f70b79f64cf51002814ef
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-06-20 20:15:35 +0530

    Change title

--- source/conf.py
+++ source/conf.py
@@ -105,7 +105,7 @@ html_theme = 'haiku'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "An Introduction to libuv"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
