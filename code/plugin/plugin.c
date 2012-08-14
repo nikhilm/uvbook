2e001e4003911c681644cfd3ef0e14706ac9d8a1
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-08-14 16:22:44 +0530

    Added dynamic loading example

---
+++ code/plugin/plugin.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void mfp_register(const char *name) {
+    fprintf(stderr, "Registered plugin \"%s\"\n", name);
+}
---
+++ code/plugin/plugin.h
@@ -0,0 +1,6 @@
+#ifndef UVBOOK_PLUGIN_SYSTEM
+#define UVBOOK_PLUGIN_SYSTEM
+
+void mfp_register(const char *name);
+
+#endif
