d62343bd8e42ab67fcb4a2f5c20f95dadec22834
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-12-25 13:29:16 -0800

    Added signals section and example

---
+++ code/signal/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <uv.h>
+
+void signal_handler(uv_signal_t *handle, int signum)
+{
+    printf("Signal received: %d\n", signum);
+    uv_signal_stop(handle);
+}
+
+// two signal handlers in one loop
+void thread1_worker(void *userp)
+{
+    uv_loop_t *loop1 = uv_loop_new();
+
+    uv_signal_t sig1a, sig1b;
+    uv_signal_init(loop1, &sig1a);
+    uv_signal_start(&sig1a, signal_handler, SIGUSR1);
+
+    uv_signal_init(loop1, &sig1b);
+    uv_signal_start(&sig1b, signal_handler, SIGUSR1);
+
+    uv_run(loop1);
+}
+
+// two signal handlers, each in its own loop
+void thread2_worker(void *userp)
+{
+    uv_loop_t *loop2 = uv_loop_new();
+    uv_loop_t *loop3 = uv_loop_new();
+
+    uv_signal_t sig2;
+    uv_signal_init(loop2, &sig2);
+    uv_signal_start(&sig2, signal_handler, SIGUSR1);
+
+    uv_signal_t sig3;
+    uv_signal_init(loop3, &sig3);
+    uv_signal_start(&sig3, signal_handler, SIGUSR1);
+
+    while (uv_run_once(loop2) || uv_run_once(loop3)) {
+    }
+}
+
+int main()
+{
+    printf("PID %d\n", getpid());
+
+    uv_thread_t thread1, thread2;
+
+    uv_thread_create(&thread1, thread1_worker, 0);
+    uv_thread_create(&thread2, thread2_worker, 0);
+
+    uv_thread_join(&thread1);
+    uv_thread_join(&thread2);
+    return 0;
+}
