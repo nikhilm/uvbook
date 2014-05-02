1728c05c3306ddb47b9843bcc3afe997c66b9ed4
Errol Chevannes <errol@malooba.com>
2014-05-02 13:30:56 +0100

    Updated example of creating a new loop.
    Using uv_loop_new() to create a new event loop is now deprecated. 
    
    From uv.h:
    
    /*
     * Allocates and initializes a new loop.
     * NOTE: This function is DEPRECATED (to be removed after 0.12), users should
     * allocate the loop manually and use uv_loop_init instead.
     */
    UV_EXTERN uv_loop_t* uv_loop_new(void);

--- code/helloworld/main.c
+++ code/helloworld/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <uv.h>
 
 int main() {
-    uv_loop_t *loop = uv_loop_new();
-
+    uv_loop_t *loop = malloc(sizeof(uv_loop_t));
+    uv_loop_init(loop);
+  
     printf("Now quitting.\n");
     uv_run(loop, UV_RUN_DEFAULT);
 
