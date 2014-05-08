2965d07ecf8fe49706149621c4e743a86f201800
Nikhil Marathe <nsm.nikhil@gmail.com>
2014-05-07 22:00:17 -0700

    Fix bad commit. uvtee is now updated

--- code/uvtee/main.c
+++ code/uvtee/main.c
@@ -16,8 +16,11 @@ uv_pipe_t stdin_pipe;
 uv_pipe_t stdout_pipe;
 uv_pipe_t file_pipe;
 
-uv_buf_t alloc_buffer(uv_handle_t *handle, size_t suggested_size) {
-    return uv_buf_init((char*) malloc(suggested_size), suggested_size);
+void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t* buf) {
+    buf->base = (char*) malloc(suggested_size);
+    if (!buf->base) {
+        buf->len = 0;
+    }
 }
 
 void free_write_req(uv_write_t *req) {
@@ -26,37 +29,35 @@ void free_write_req(uv_write_t *req) {
     free(wr);
 }
 
-void on_stdout_write(uv_write_t *req, int status) {
+void on_write(uv_write_t *req, int status) {
     free_write_req(req);
 }
 
-void on_file_write(uv_write_t *req, int status) {
-    free_write_req(req);
-}
-
-void write_data(uv_stream_t *dest, size_t size, uv_buf_t buf, uv_write_cb callback) {
+void write_data(uv_stream_t *dest, size_t size, const uv_buf_t *buf, uv_write_cb callback) {
     write_req_t *req = (write_req_t*) malloc(sizeof(write_req_t));
     req->buf = uv_buf_init((char*) malloc(size), size);
-    memcpy(req->buf.base, buf.base, size);
+    memcpy(req->buf.base, buf->base, size);
     uv_write((uv_write_t*) req, (uv_stream_t*)dest, &req->buf, 1, callback);
 }
 
-void read_stdin(uv_stream_t *stream, ssize_t nread, uv_buf_t buf) {
-    if (nread == -1) {
-        if (uv_last_error(loop).code == UV_EOF) {
+void read_stdin(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
+    if (nread < 0) {
+        if (nread == UV_EOF) {
             uv_close((uv_handle_t*)&stdin_pipe, NULL);
             uv_close((uv_handle_t*)&stdout_pipe, NULL);
             uv_close((uv_handle_t*)&file_pipe, NULL);
+        } else {
+            fprintf(stderr, "Error %s\n", uv_strerror(nread));
         }
     }
     else {
         if (nread > 0) {
-            write_data((uv_stream_t*)&stdout_pipe, nread, buf, on_stdout_write);
-            write_data((uv_stream_t*)&file_pipe, nread, buf, on_file_write);
+            write_data((uv_stream_t*)&stdout_pipe, nread, buf, on_write);
+            write_data((uv_stream_t*)&file_pipe, nread, buf, on_write);
         }
     }
-    if (buf.base)
-        free(buf.base);
+    if (buf->base)
+        free(buf->base);
 }
 
 int main(int argc, char **argv) {
