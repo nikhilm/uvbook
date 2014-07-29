8389834deeb7729e313e40c3f173334869c0774b
TonyAdo <coolhzb@163.com>
2014-07-29 10:40:38 +0800

    fix the uvcat/main bugs....

--- code/uvcat/main.c
+++ code/uvcat/main.c
@@ -16,15 +16,17 @@ static uv_buf_t iov;
 void on_write(uv_fs_t *req) {
     uv_fs_req_cleanup(req);
     if (req->result < 0) {
-        fprintf(stderr, "Write error: %s\n", uv_strerror(uv_last_error(uv_default_loop())));
+        fprintf(stderr, "Write error: %s\n", uv_strerror((int)req->result));
     }
     else {
-        uv_fs_read(uv_default_loop(), &read_req, open_req.result, buffer, sizeof(buffer), -1, on_read);
+        printf("Write done\n");
+        uv_fs_read(uv_default_loop(), &read_req, open_req.result, &iov, 1, -1, on_read);
     }
 }
 
 void on_read(uv_fs_t *req) {
     uv_fs_req_cleanup(req);
+    printf("read content: %s\n", iov.base);
     if (req->result < 0) {
         fprintf(stderr, "Read error: %s\n", uv_strerror((int)req->result));
     }
@@ -34,7 +36,7 @@ void on_read(uv_fs_t *req) {
         uv_fs_close(uv_default_loop(), &close_req, open_req.result, NULL);
     }
     else {
-        uv_fs_write(uv_default_loop(), &write_req, 1, buffer, req->result, -1, on_write);
+        uv_fs_write(uv_default_loop(), &write_req, req->result, &iov, 1, -1, on_write);
     }
 }
 
