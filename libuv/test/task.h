b71118a2c7df4261a5d49b133b57792b6a01cbe1
Vinci <root@vinci.pro>
2016-12-21 06:13:38 +0800

    fix when not define _MSC_VER_

--- libuv/test/task.h
+++ libuv/test/task.h
@@ -186,7 +186,7 @@ enum test_status {
 #endif
 
 
-#if defined _WIN32 && ! defined __GNUC__ && _MSC_VER < 1900
+#if defined _WIN32 && ! defined __GNUC__ && defined(_MSC_VER) && _MSC_VER < 1900
 
 #include <stdarg.h>
 
